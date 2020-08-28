9dd0dde8c051ebfb0ac6d6a3171acee054f442f2
Comoc <e@w.com>
2020-08-28 11:30:30 +0300

    instal origin

---
+++ common/config/db.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Semenov Alexander <support@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 17.09.2015
+ */
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=stroyhors',
+            'username' => 'shop',
+            'password' => '2208725c',
+			//'dsn' => 'mysql:host=localhost;dbname=app_basic_smarty',
+            //'username' => 'app',
+           // 'password' => 'aPGb5ByZKPod1WWI',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 3600,
+        ]
+    ]
+];
